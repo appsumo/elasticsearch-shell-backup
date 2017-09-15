8706f24298e5e3564c82e72ddf5ae445ac4c3f2b
Dr. Ogg <ogg@sr375.com>
2017-09-15 12:13:40 -0700

    dont wait for snapshot to finish

--- elasticsearch-take-snapshot.sh
+++ elasticsearch-take-snapshot.sh
@@ -3,5 +3,4 @@
 base_dir="$(dirname "$0")"
 source $base_dir/shell-variables
 
-curl -XPUT "$URL/_snapshot/$REPO/$SNAPSHOT?wait_for_completion=true" | jq '.'
-
+curl -XPUT "$URL/_snapshot/$REPO/$SNAPSHOT?wait_for_completion=false" | jq '.'
